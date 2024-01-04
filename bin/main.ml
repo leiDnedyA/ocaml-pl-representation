44a485095afa376ec313d11cb457b6522c1a39e5
leiDnedyA <aydendiel@gmail.com>
2024-01-04 16:50:58 -0500

    feat: added comments with notes from meeting about expressions, statements, and programs

--- bin/main.ml
+++ bin/main.ml
@@ -5,6 +5,36 @@
 (* hash table : ["x":1] ["y":200] *)
 (* (define x 1) *)
 
+(*
+(* Notes from Jan 4 meeting w Ramsey *)
+type expression =
+  | int
+  | var
+  | True
+  | False
+  | Add # <and all other arithmetical expressions
+  | And # <and all other boolean operators>
+
+type statement =
+  | Assign string * expression
+  | If expression * int
+  | Goto int # the purpose of a goto is express a while loop as an if statement
+
+let rec eval_statement env pc stmt = # env -> hashtable, pc -> program counter (line number), stmt -> statement
+  (*evaluate a statement -> return an updated env and updated pc*)
+  match stmt with
+  | Assign(str, expr) ->
+      (*Update the env*)
+      (env, pc + 1)
+  | If (expr, pc) -> (*Make sure expr is boolean (2 < 5) (true && false)*)
+      (*If expr evaluates to true, jump to pc*)
+      (*Else, increment pc by 1*)
+  | Goto(pc) -> pc
+
+  let evaluate_program (program:statement list) = 
+
+ *)
+
 let hashtable_size = 25;;
 
 type value =
