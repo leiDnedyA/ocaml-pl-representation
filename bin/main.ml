c8973b660e243708a511fc54aef26e692527b355
leiDnedyA <aydendiel@gmail.com>
2024-01-03 20:31:05 -0500

    Tweaked Add constructor and eval_add to take two arguments instead of list of arguments

--- bin/main.ml
+++ bin/main.ml
@@ -13,7 +13,9 @@ type value =
 ;;
       
 type operation =
-  |Add of value list
+  |Multiply of value * value
+  |Subtract of value * value
+  |Add of value * value
   |Define of value * value
 ;;
 
@@ -23,28 +25,34 @@ let lookup_var name = Hashtbl.find vars_hashtable name;;
 let create_var name value = Hashtbl.add vars_hashtable name value;;
 
 
-let rec eval_add = function
-  | [] -> 0
-  | [Int a] -> a
-  | [Var a] -> lookup_var a
-  | Int first :: rest -> first + (eval_add rest)
-  | Var first :: rest -> (lookup_var first) + (eval_add rest)
+let eval_add = function
+  | Add (Int a, Int b) -> a + b
+  | Add (Var a, Int b) -> (lookup_var a) + b
+  | Add (Int a, Var b) -> (lookup_var b) + a
+  | Add (Var a, Var b) -> (lookup_var a) + (lookup_var b)
+  | Multiply (_, _) -> failwith("Invalid operation type for eval_add")
+  | Subtract (_, _) -> failwith("Invalid operation type for eval_add")
+  | Define (_, _) -> failwith("Invalid operation type for eval_add")
+;;
 
 let eval_operation expr = 
   match expr with
-  | Add a -> eval_add a
+  | Add (a, b) -> eval_add (Add (a, b))
   | Define (Var var_name, Int var_value) ->
       create_var var_name var_value; 0
+  | Subtract (_, _) -> failwith("Not yet implemented")
+  | Multiply (_, _) -> failwith("Not yet implemented")
   | Define (_, _) -> failwith("Invalid define operation")
 ;;
 
 
 let define_test = Define (Var "x", Int 2);;
-let add_test = Add [Int 2; Var "x";];;
+let add_test = Add (Int 2, Var "x";);;
 
 eval_operation define_test;;
 Printf.printf "%d\n" (lookup_var "x");;
 
 Printf.printf "%d\n" (eval_operation add_test);;
 
-
+Multiply (Int 1, Int 2);;
+Subtract (Int 1, Int 2);;
